1eeead328ac254dc02a1f9c952103505a86eabd6
z0ntik02 <hfhfhfhhjrj@gmail.com>
2023-09-23 01:01:57 +0500

    PZ_05

---
+++ PZ_05/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double x = -2.5;
+           
+            while(x < 2)
+            {
+                x += 0.5;
+                double y = -2.4 * Math.Pow(x, 2) + 5 * x - 3;
+                Console.WriteLine($" x = {x} , y = {Math.Round(y,2)}");
+            }
+        }
+    }
+}
