cb9044826e9769040627f6ffd66bf9aa862373d0
z0ntik02 <hfhfhfhhjrj@gmail.com>
2023-11-28 14:35:59 +0500

    PZ_12

---
+++ PZ_12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//определение размерности геометрической прогрессии
+            GeomProg(n);// использование метода
+        }
+        public static double[] GeomProg(int n)//метод геометрической прогрессии
+        {
+            double[] result = new double[n];//создание массива членов геометрической прогрессии
+            for (int i = 0; i < n; i++)//цикл для заполнения массива 
+            {
+                result[i] = 10 * Math.Pow(2, i);//вычисление каждого элемента прогрессии
+                Console.Write(result[i] + " ");//вывод членов прогрессии
+            }
+            return result;
+        }
+    }
+}
