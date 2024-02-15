910127492ab935fd893b72c38e35b62bc34a11fe
z0ntik02 <hfhfhfhhjrj@gmail.com>
2024-02-15 20:51:42 +0500

    PZ_18

---
+++ PZ_18/PZ_18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rnd = new Random();
+            
+
+            DateTime EndData = new DateTime(rnd.Next(2024,2026), rnd.Next(3,12), rnd.Next(1,30));
+            Ticket ticket = new Ticket(TicketCategory.Business, 5, 18, "Иванов Иван Иванович", EndData);
+            Console.WriteLine();
+            DateTime EndData2 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket2 = new Ticket(TicketCategory.Economy, 7, 1, "", EndData2);
+            Console.WriteLine();
+            DateTime EndData3 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket3 = new Ticket(TicketCategory.Business, 35, 31, "Петрова Наталья Евгеньевна", EndData3);
+            Console.WriteLine();
+            DateTime EndData4 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket4 = new Ticket(TicketCategory.Economy, 2, 90, "Петров Петр Петрович", EndData4);
+            Console.WriteLine();
+            DateTime EndData5 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket5 = new Ticket(TicketCategory.Economy, 9, 78, "Алексеев Алексей Алексеевич", EndData5);
+            Console.WriteLine();
+            DateTime EndData6 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket6 = new Ticket(TicketCategory.Economy, 11, 56, "Антонов Антон Антонович", EndData6);
+            Console.WriteLine();
+            DateTime EndData7 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket7 = new Ticket(TicketCategory.Economy, 16, 24, "Александрова Александра Александровна", EndData7);
+            Console.WriteLine();
+            DateTime EndData8 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket8 = new Ticket(TicketCategory.Business, 19, 2, "Сидоров Олег Александрович", EndData8);
+            Console.WriteLine();
+            DateTime EndData9 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket9 = new Ticket(TicketCategory.Economy, 24, 13, "Карасев Артем Германович", EndData9);
+            Console.WriteLine();
+            DateTime EndData10 = new DateTime(rnd.Next(2024, 2026), rnd.Next(1, 12), rnd.Next(1, 30));
+            Ticket ticket10 = new Ticket(TicketCategory.Business, 29, 5, "", EndData10);
+            
+
+
+
+        }
+    }
+}
---
+++ PZ_18/PZ_18/Ticket.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_18
+{
+    public enum TicketCategory
+    {
+        Business,
+        Economy
+    }
+
+    public class Ticket
+    {
+        public TicketCategory Category { get; set; }
+        public int SeatNumber { get; set; }
+        public string PassengerName { get; set; }
+
+        public DateTime DateDeparture { get; set; }
+        
+        public int DateOnly { get; set; }
+        public double TicketPrice { get; private set; }
+
+        private static int EconomyTicketsSold = 0;
+        private static int BusinessTicketsSold = 0;
+
+        public Ticket(TicketCategory Category, int seat, int date, string Name,DateTime data)
+        {
+            
+                if (string.IsNullOrWhiteSpace(Name))
+                {
+                    Console.WriteLine("Введите корректные данные");
+                    PassengerName = Console.ReadLine();
+                }
+                else
+                    PassengerName = Name;
+
+                if (Category == TicketCategory.Economy)
+                {
+                    TicketPrice = 5000;
+                    EconomyTicketsSold++;
+                    if(EconomyTicketsSold > 30) 
+                        throw new InvalidOperationException("В выбранном классе мест нет");
+
+                }
+                else
+                {
+                    TicketPrice = 10000;
+                    BusinessTicketsSold++;
+                    if (BusinessTicketsSold >7)
+                        throw new InvalidOperationException("В выбранном классе мест нет");
+                }
+                if (DateOnly < 10)
+                    TicketPrice /= 2;
+                DateDeparture = data;
+                SeatNumber = seat;
+                TicketInfo();
+                TimeToDepart();
+                SoldTickets();
+
+        }
+
+        public void TicketInfo()
+        {
+            Console.WriteLine($"ФИО: {PassengerName}");
+            Console.WriteLine($"Место: {SeatNumber}");
+            Console.WriteLine($"Дата и время вылета:{DateDeparture} ");
+            Console.WriteLine($"Класс: {Category}");
+            Console.WriteLine($"Стоимость билета: {TicketPrice}");
+        }
+        public void TimeToDepart()
+        {
+            Console.WriteLine($"Время до вылета: {DateDeparture - DateTime.Now}");
+        }
+        public static void SoldTickets()
+        {
+            Console.WriteLine($"Продано билетов Эконом класса: {EconomyTicketsSold}");
+            Console.WriteLine($"Продано билетов Бизнес класса: {BusinessTicketsSold}");
+        }
+            
+
+    }
+    
+}
